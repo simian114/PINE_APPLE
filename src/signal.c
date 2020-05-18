8a4df72298ed3a6a03eb337dd8f06cd458d92bb6
nam sang hyeok <simian114@gmail.com>
2020-05-18 17:25:21 +0900

    Sanam, Handle SIGINT

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 02:19:55 by gmoon             #+#    #+#             */
-/*   Updated: 2020/05/18 12:08:51 by sanam            ###   ########.fr       */
+/*   Updated: 2020/05/18 17:24:36 by sanam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 void		sigint_handle()
 {
-//	ft_putstr_fd("\n", 1);
-//	ft_putendl_fd("SIGINT?", 1);
+	ft_putstr_fd("\e[2D\e[0K", 1);
 	ft_putendl_fd("", 1);
 	print_commandline();
 }
