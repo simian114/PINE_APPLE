ece99bc4b0cd23c7deadb841dd64d9b75a87e325
nam <simian114@gmail.com>
2020-05-18 12:18:10 +0900

    2020. 05. 18. SIGNAL

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 02:19:55 by gmoon             #+#    #+#             */
-/*   Updated: 2020/05/12 02:20:07 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/05/18 12:08:51 by sanam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void		sigint_handle()
 {
-	ft_putstr_fd("\n", 1);
-	ft_putendl_fd("SIGINT?", 1);
+//	ft_putstr_fd("\n", 1);
+//	ft_putendl_fd("SIGINT?", 1);
+	ft_putendl_fd("", 1);
 	print_commandline();
 }
 
