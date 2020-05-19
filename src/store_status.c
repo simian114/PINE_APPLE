498457a7cdf2f90b27456ae28a42e11bcecdb455
nam sang hyeok <simian114@gmail.com>
2020-05-19 13:19:40 +0900

    2020. 05. 19. Make PINE_APPLE store ? env

---
+++ src/store_status.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   store_status.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanam <sanam@studenr.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/19 13:06:41 by sanam             #+#    #+#             */
+/*   Updated: 2020/05/19 13:06:59 by sanam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void		store_status(t_list *envs, int *wstatus)
+{
+	char	*status;
+	char	*str;
+	char	*args[2];
+
+	status = ft_itoa(*wstatus);
+	str = ft_strjoin("?=", status);
+	args[0] = ft_strdup(str);
+	args[1] = NULL;
+	sh_export(args, envs);
+	free(status);
+	free(str);
+}
