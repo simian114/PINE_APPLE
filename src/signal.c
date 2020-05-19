67a5d3f19dfa0928c59ecdd75cd74ad8c91976fe
nam sang hyeok <simian114@gmail.com>
2020-05-19 17:57:35 +0900

    2020. 05. 19. NOTHING....

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 02:19:55 by gmoon             #+#    #+#             */
-/*   Updated: 2020/05/18 17:24:36 by sanam            ###   ########.fr       */
+/*   Updated: 2020/05/19 15:34:20 by sanam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,5 @@ void		sigint_handle()
 
 void		sigquit_handle()
 {
-	ft_putstr_fd("\n", 1);
-	ft_putendl_fd("SIGQUIT?", 1);
-	print_commandline();
+	ft_putstr_fd("\e[2D\e[0K", 1);
 }
