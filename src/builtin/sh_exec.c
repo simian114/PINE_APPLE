6410e36e072b1ec129f93cd9014104653a6c4aac
moon9ua <koom6@naver.com>
2020-05-21 12:53:59 +0900

    gmoon: sh_exec 누수 수정

--- src/builtin/sh_exec.c
+++ src/builtin/sh_exec.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 02:20:53 by gmoon             #+#    #+#             */
-/*   Updated: 2020/05/21 12:17:57 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/05/21 12:52:50 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,36 +50,46 @@ void		sh_exec(char **args, char **envp, t_list *envs)
 	char	*path;
 	char	*program_tmp;
 	char	*program;
+	char	*env_path_mover;
 
 	if (ft_strncmp(*args, "/", 1) == 0 || ft_strncmp(*args, ".", 1) == 0)
+	{
 		ret = exec_fork(args[0], args, envp);
+		if (ret == -1)
+		{
+			ft_putstr_fd("\033[3m\033[31mPINE_APPLE:\033[0m ", 2);
+			ft_putstr_fd(strerror(errno), 2);
+			ft_putstr_fd(": ", 2);
+			ft_putendl_fd(*args, 2);
+			exit(1);
+		}
+	}
 	else
 	{
 		if (find_value(envs, "PATH"))
-			env_path = ft_strdup(find_value(envs, "PATH"));
-		while (1)
 		{
-			int len;
-
-			len = path_len(env_path);
-			path = ft_substr(env_path, 0, len);
-			if (len == 0)
+			env_path = ft_strdup(find_value(envs, "PATH"));
+			env_path_mover = env_path;
+			while (*env_path_mover)
 			{
-				free(path);
-				break ;
+				path = ft_substr(env_path_mover, 0, path_len(env_path_mover));
+				env_path_mover += path_len(env_path_mover) + 1;
+				program_tmp = ft_strjoin_free(path, char_to_str('/'));
+				program = ft_strjoin_s1free(program_tmp, *args);
+				ret = exec_fork(program, args, envp);
+				free(program);
+				if (ret >= 0)
+					exit(0);
 			}
-			env_path += len + 1;
-			program_tmp = ft_strjoin_free(path, char_to_str('/'));
-			program = ft_strjoin_s1free(program_tmp, *args);
-			ret = exec_fork(program, args, envp);
-			free(program);
-			if (ret >= 0)
-				exit(0);
+			free(env_path);
+			free(path);
 		}
-		ft_putstr_fd("\033[3m\033[31mPINE_APPLE:\033[0m command not found: ", 2);
+		ft_putstr_fd("\033[3m\033[31mPINE_APPLE:\033[0m ", 2);
+		ft_putstr_fd("command not found: ", 2);
 		ft_putendl_fd(*args, 2);
 		exit(1);
 	}
+
 	// printf("%s\n", env_path);
 	
 	// ret = exec_fork(args, envp);
