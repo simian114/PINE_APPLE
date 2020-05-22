f9cc084fe3cf25aa18701aa6db1b545c4d70c25f
sanam <simian114@gmail.com>
2020-05-22 21:01:49 +0900

    2020. 05. 22. Quote

--- src/util.c
+++ src/util.c
@@ -6,26 +6,62 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/11 18:39:20 by gmoon             #+#    #+#             */
-/*   Updated: 2020/05/22 16:04:13 by sanam            ###   ########.fr       */
+/*   Updated: 2020/05/22 21:01:21 by sanam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int		escape(char **line, int *quote)
+{
+	char	buf[3];
+
+	ft_bzero(buf, 3);
+	*buf = '\\';
+	read(0, buf + 1, 1);
+	if (*quote == '\'')
+		*line = ft_strjoin_s1free(*line, buf);
+	else if (*quote == '\"')
+	{
+		if (buf[1] == '$' || buf[1] == '`' || buf[1] == '"' || buf[1] == '\\')
+		{
+			if (buf[1] == '$')
+				buf[1] = -20;
+			*line = ft_strjoin_s1free(*line, buf + 1);
+		}
+		else
+			*line = ft_strjoin_s1free(*line, buf);
+	}
+	else
+		*line = ft_strjoin_s1free(*line, buf + 1);
+	return (1);
+}
+
+void	quoteing(int *quote, char buf)
+{
+	if ((buf == '\'' || buf == '\"') && *quote == 0)
+		*quote = buf;
+	else if ((buf == '\'' || buf == '\"') && *quote != 0)
+		*quote = (*quote == buf) ? 0 : *quote;
+}
+
 int		get_line(char **line)
 {
 	char	buf[2];
-	char	*temp;
+	int		quote;
 
 	ft_bzero(buf, 2);
 	*line = ft_strdup("");
+	quote = 0;
 	while (*buf != '\n')
 	{
 		while (read(0, buf, 1) && *buf != '\n')
 		{
-			temp = *line;
-			*line = ft_strjoin(*line, buf);
-			free(temp);
+			if (*buf == '\'' || *buf == '\"')
+				quoteing(&quote, *buf);
+			if (*buf == '\\' && escape(line, &quote))
+				continue ;
+			*line = ft_strjoin_s1free(*line, buf);
 		}
 		if (ft_strlen(*line) == 0 && *buf != '\n')
 			return (0);
