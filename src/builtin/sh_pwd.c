04f87838fc6524448cec8eed6787dc68980b6501
moon9ua <koom6@naver.com>
2020-05-23 23:55:11 +0900

    gmoon: norm

--- src/builtin/sh_pwd.c
+++ src/builtin/sh_pwd.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 00:17:27 by gmoon             #+#    #+#             */
-/*   Updated: 2020/05/21 06:52:42 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/05/23 23:54:57 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,10 @@
 
 void		sh_pwd(char **args, int fd)
 {
-	// int		argc;
 	char	*cwd;
 
 	if (!args)
 		exit(1);
-	// argc = get_argc(args);
-	// if (argc > 1)
-	// {
-	// 	ft_putstr_fd("pwd: ", 2);
-	// 	ft_putendl_fd("too many arguments", 2);
-	// 	exit(1);
-	// }
 	cwd = getcwd(0, 1024);
 	ft_putendl_fd(cwd, fd);
 	free(cwd);
