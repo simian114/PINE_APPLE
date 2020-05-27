92dc20becdf41ba796667801753dda75f675c08a
moon9ua <koom6@naver.com>
2020-05-27 16:53:06 +0900

    gmoon: modyfi exit clear

--- src/builtin/sh_exit.c
+++ src/builtin/sh_exit.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 22:38:45 by sanam             #+#    #+#             */
-/*   Updated: 2020/05/27 06:49:59 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/05/27 16:52:48 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,50 +44,5 @@ int			sh_exit(char **cmd)
 		ft_putstr_fd("bash: exit: numeric argument required\n", 2);
 		exit(2);
 	}
-
-	
-	// else if (i == 2)
-	// {
-	// 	if (check_numeric(cmd[1]) == 0)
-	// 		exit(ft_atoi(cmd[1]));
-	// 	else
-	// 	{
-	// 		ft_putendl_fd("bash: exit: numeric argument required", 2);
-	// 		exit(2);
-	// 	}
-	// }
-	// else
-	// {
-	// 	while (cmd[i])
-	// 	if (check_numeric(cmd))
-	// }
-	// 	ft_putendl_fd("bash: exit: too many arguments", 2);
-
-
-	// if (i == 1)
-	// 	exit(0);
-	// else if (i > 2)
-	// {
-	// 	printf("zz?\n");
-	// 	if (!check_numeric(cmd[1]))
-	// 	{
-	// 		printf("test\n");
-	// 		ft_putendl_fd("exit\nbash: exit: too many arguments", 2);
-	// 		return (1);
-	// 	}	
-	// 	ft_putstr_fd("exit\nbash: exit: ", 2);
-	// 	ft_putstr_fd(cmd[1], 2);
-	// 	ft_putendl_fd(": numeric argument required", 2);
-	// 	exit(2);
-	// }
-	// else if (i == 2)
-	// {
-	// 	if (check_numeric(cmd[1]))
-	// 		exit(2);
-	// 	else
-	// 		exit(ft_atoi(cmd[1]));
-	// }
-	// exit(i);
-
 	return (1);
 }
